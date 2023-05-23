bc82f94a6ad057301444bc93e29fb4f3ef679b0b
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-23 14:43:17 +0700

    setup jwt auth

--- Program.cs
+++ Program.cs
@@ -1,7 +1,57 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services
+    .AddHttpContextAccessor();
+
+builder.Services
+    .AddDistributedMemoryCache();
+
+builder.Services
+    .AddSession();
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.IsEssential = true;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                context.Token = context.HttpContext.Session.GetString("AccessToken");
+
+                return Task.CompletedTask;
+            }
+        };
+        options.TokenValidationParameters = new()
+        {
+            ClockSkew = TimeSpan.Zero,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration.GetValue<string>("JWT:Issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("JWT:Audience"),
+            IssuerSigningKey =
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JWT:Key")))
+        };
+        options.ForwardSignIn = "/Login";
+        options.SaveToken = true;
+    });
+builder.Services
+    .AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +68,9 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
