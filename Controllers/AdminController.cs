60153e7e3ca8266e837f92433fd68630badb5c64
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-30 13:42:32 +0700

    implement claim item and confirm claimed

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,6 @@
 using System.Diagnostics;
+using System.Security.Claims;
+using Client.Controllers.common;
 using Microsoft.AspNetCore.Mvc;
 using Client.Models;
 using Client.Extensions;
@@ -8,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 namespace Client.Controllers;
 
 [Route("[controller]")]
+[Authorize(Roles = "Admin")]
 public class AdminController : Controller
 {
     private readonly RESTAPIService _restapiService;
@@ -20,7 +23,23 @@ public class AdminController : Controller
     [Route("Dashboard")]
     public async Task<IActionResult> Dashboard()
     {
-        return View();
+        var pendingRequestFound = await _restapiService.Get<Response<IEnumerable<RequestFoundItem>>>(QueryHelpers.AddQueryString("items/request-found", "status", "pending"), HttpContext.Session.GetString("AccessToken"));
+        var pendingRequestClaim = await _restapiService.Get<Response<IEnumerable<RequestClaimItem>>>(QueryHelpers.AddQueryString("items/request-claim", "status", "pending"), HttpContext.Session.GetString("AccessToken"));
+        return View(new Tuple<int, int>(pendingRequestFound.Data.Count(), pendingRequestClaim.Data.Count()));
+    }
+    
+    [Route("Profile")]
+    public async Task<IActionResult> Profile()
+    {
+        var employeeData = await _restapiService.Get<Response<Employee>>($"employees/{User.FindFirstValue(ClaimTypes.Sid)}", HttpContext.Session.GetString("AccessToken"));
+        var departmentsData = await _restapiService.Get<Response<IEnumerable<Department>>>($"departments", HttpContext.Session.GetString("AccessToken"));
+        var genderForms = new List<GenderForm>
+        {
+            new GenderForm("Female", "F"),
+            new GenderForm("Male", "M")
+        };
+        
+        return View(new Tuple<Employee, IEnumerable<Department>, IEnumerable<GenderForm>>(employeeData.Data, departmentsData.Data, genderForms));
     }
 
     [Route("Items/Found")]
@@ -28,13 +47,44 @@ public class AdminController : Controller
     {
         var response = await _restapiService.Get<Response<IEnumerable<FoundItem>>>("items/found", HttpContext.Session.GetString("AccessToken"));
 
+        if (Request.Headers["Accept"].Equals("application/json"))
+        {
+            return Ok(response.Data);
+        }
+
         return View("Items/Found", response.Data);
+    }
+    
+    [Route("Items/Found/All")]
+    public async Task<IActionResult> FoundItemsAll()
+    {
+        var response = await _restapiService.Get<Response<IEnumerable<FoundItem>>>("items/found/all", HttpContext.Session.GetString("AccessToken"));
+
+        if (Request.Headers["Accept"].Equals("application/json"))
+        {
+            return Ok(response.Data);
+        }
+
+        return View("Items/Found", response.Data);
+    }
+    
+    [Route("Items/Claimed")]
+    public async Task<IActionResult> ClaimedItems()
+    {
+        var response = await _restapiService.Get<Response<IEnumerable<ClaimedItem>>>("items/claimed", HttpContext.Session.GetString("AccessToken"));
+        
+        if (Request.Headers["Accept"].Equals("application/json"))
+        {
+            return Ok(response.Data);
+        }
+
+        return View("Items/Claimed", response.Data);
     }
 
     [Route("Items/RequestFound")]
     public async Task<IActionResult> RequestFoundItems()
     {
-        var response = await _restapiService.Get<Response<IEnumerable<RequestFound>>>(QueryHelpers.AddQueryString("items/request-found", Request.Query), HttpContext.Session.GetString("AccessToken"));
+        var response = await _restapiService.Get<Response<IEnumerable<RequestFoundItem>>>(QueryHelpers.AddQueryString("items/request-found", Request.Query), HttpContext.Session.GetString("AccessToken"));
 
         return View("Items/RequestFound", response.Data);
     }
@@ -42,7 +92,7 @@ public class AdminController : Controller
     [Route("Items/RequestClaim")]
     public async Task<IActionResult> RequestClaimItems()
     {
-        var response = await _restapiService.Get<Response<IEnumerable<RequestClaim>>>(QueryHelpers.AddQueryString("items/request-claim", Request.Query), HttpContext.Session.GetString("AccessToken"));
+        var response = await _restapiService.Get<Response<IEnumerable<RequestClaimItem>>>(QueryHelpers.AddQueryString("items/request-claim", Request.Query), HttpContext.Session.GetString("AccessToken"));
 
         return View("Items/RequestClaim", response.Data);
     }
@@ -51,37 +101,57 @@ public class AdminController : Controller
     [Route("Items/RequestFound/Approve")]
     public async Task<IActionResult> ProcessApproveRequestFound([FromForm] updateRequest request)
     {
-        var response = await _restapiService.Patch<UpdateRequest, Response<Status>>($"items/request-found/{request.RequestId}/approve", new UpdateRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
+        var response = await _restapiService.Patch<UpdateStatusRequest, Response<Status>>($"items/request-found/{request.RequestId}/approve", new UpdateStatusRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
 
-        return RedirectToAction(nameof(RequestFoundItems));
+        return RedirectToAction("RequestFoundItems");
     }
     
     [HttpPost]
     [Route("Items/RequestFound/Reject")]
     public async Task<IActionResult> ProcessRejectRequestFound([FromForm] updateRequest request)
     {
-        var response = await _restapiService.Patch<UpdateRequest, Response<Status>>($"items/request-found/{request.RequestId}/reject", new UpdateRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
+        var response = await _restapiService.Patch<UpdateStatusRequest, Response<Status>>($"items/request-found/{request.RequestId}/reject", new UpdateStatusRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
 
-        return RedirectToAction(nameof(RequestFoundItems));
+        return RedirectToAction("RequestFoundItems");
     }
     
     [HttpPost]
     [Route("Items/RequestClaim/Approve")]
     public async Task<IActionResult> ProcessApproveRequestClaim([FromForm] updateRequest request)
     {
-        await _restapiService.Patch<UpdateRequest, Response<Status>>($"items/found/request-claim/{request.RequestId}/approve", new UpdateRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
+        await _restapiService.Patch<UpdateStatusRequest, Response<Status>>($"items/found/request-claim/{request.RequestId}/approve", new UpdateStatusRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
 
-        return RedirectToAction(nameof(RequestClaimItems));
+        return RedirectToAction("RequestClaimItems");
     }
     
     [HttpPost]
     [Route("Items/RequestClaim/Reject")]
     public async Task<IActionResult> ProcessRejectRequestClaim([FromForm] updateRequest request)
     {
-        await _restapiService.Patch<UpdateRequest, Response<Status>>($"items/found/request-claim/{request.RequestId}/reject", new UpdateRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
+        await _restapiService.Patch<UpdateStatusRequest, Response<Status>>($"items/found/request-claim/{request.RequestId}/reject", new UpdateStatusRequest{Message = request.Message}, HttpContext.Session.GetString("AccessToken"));
 
-        return RedirectToAction(nameof(RequestClaimItems));
+        return RedirectToAction("RequestClaimItems");
+    }
+
+    [HttpPost]
+    [Route("Items/ConfirmFound")]
+    public async Task<IActionResult> ProcessConfirmFound([FromForm] confirmRequest request)
+    {
+        var response = await _restapiService.Post<Response<ItemAction>>($"items/request-found/{request.RequestId}/confirm-found", HttpContext.Session.GetString("AccessToken"));
+
+        return RedirectToAction("RequestFoundItems");
+    }
+    
+    [HttpPost]
+    [Route("Items/ConfirmClaimed")]
+    public async Task<IActionResult> ProcessConfirmClaimed([FromForm] confirmRequest request)
+    {
+        var response = await _restapiService.Post<Response<ItemAction>>($"items/request-claim/{request.RequestId}/confirm-claimed", HttpContext.Session.GetString("AccessToken"));
+
+        return RedirectToAction("RequestClaimItems");
     }
 
     public record updateRequest(Guid RequestId, string Message);
+
+    public record confirmRequest(Guid RequestId);
 }
