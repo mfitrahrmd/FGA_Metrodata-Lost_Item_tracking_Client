77fc141d6ba28fa0c411beb8042e8dfd6233cb6d
Muhamad Fitrah Ramadhan <tgcfitrah26@gmail.com>
2023-05-30 13:41:20 +0700

    add register model

--- Models/Auth.cs
+++ Models/Auth.cs
@@ -8,3 +8,21 @@ public class LoginResponse
 {
     public string AccessToken { get; set; }
 }
+
+public class RegisterRequest
+{
+    public string Nik { get; set; }
+    public string FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime BirthDate { get; set; }
+    public string Gender { get; set; }
+    public string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public Guid DepartmentId { get; set; }
+    public string Password { get; set; }
+}
+
+public class RegisterResponse
+{
+    public string Nik { get; set; }
+}
